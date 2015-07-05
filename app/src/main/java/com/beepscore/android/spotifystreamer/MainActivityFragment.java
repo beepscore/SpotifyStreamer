a866e3b7f5ac729d9098079a311cc95289e7253e
beepscore <support@beepscore.com>
2015-07-05 15:08:13 -0700

    feat: Show list of fake artist names.
    Add layout/list_item with a text view. In MainActivityFragment add adapter.

--- app/src/main/java/com/beepscore/android/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/beepscore/android/spotifystreamer/MainActivityFragment.java
@@ -5,19 +5,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
- * A placeholder fragment containing a simple view.
+ * Shows artists
  */
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
     }
 
+    ArrayAdapter<String> adapter = null;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View artistsView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        List<String> list = new ArrayList<String>();
+
+        // TODO: replace fake list elements
+        list.add("foo");
+        list.add("bar");
+        list.add(getActivity().getString(R.string.baz));
+        list.add(getActivity().getString(R.string.dash));
+        list.add(getActivity().getString(R.string.dash));
+
+        // adapter creates views for each list item
+        adapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item,
+                R.id.list_item_textview,
+                list);
+
+        ListView listView = (ListView)artistsView.findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
+        return artistsView;
     }
 }
