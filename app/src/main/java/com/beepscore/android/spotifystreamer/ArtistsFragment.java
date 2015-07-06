e4cb26c1d80791a85c4298dbd0c03511529ce5bb
beepscore <support@beepscore.com>
2015-07-05 21:00:52 -0700

    feat: Search Spotify for artists matching "Beyonce" and display results.

--- app/src/main/java/com/beepscore/android/spotifystreamer/ArtistsFragment.java
+++ app/src/main/java/com/beepscore/android/spotifystreamer/ArtistsFragment.java
@@ -1,5 +1,6 @@
 package com.beepscore.android.spotifystreamer;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +11,11 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
 
 
 /**
@@ -29,12 +35,7 @@ public class ArtistsFragment extends Fragment {
         View artistsView = inflater.inflate(R.layout.fragment_artists, container, false);
 
         List<String> list = new ArrayList<String>();
-
-        // TODO: replace fake list elements
-        list.add("foo");
-        list.add("bar");
-        list.add(getActivity().getString(R.string.baz));
-        list.add(getActivity().getString(R.string.dash));
+        // add a placeholder element
         list.add(getActivity().getString(R.string.dash));
 
         // adapter creates views for each list item
@@ -43,9 +44,69 @@ public class ArtistsFragment extends Fragment {
                 R.id.list_item_textview,
                 list);
 
-        ListView listView = (ListView)artistsView.findViewById(R.id.list_view);
+        ListView listView = (ListView) artistsView.findViewById(R.id.list_view);
         listView.setAdapter(adapter);
 
+        fetchArtists("Beyonce");
+
         return artistsView;
+    }
+
+    private void fetchArtists(String artistName) {
+        FetchArtistsTask fetchArtistsTask = new FetchArtistsTask();
+        fetchArtistsTask.execute(artistName);
+    }
+
+////////////////////////////////////////////////////////////////////////////
+
+    // first parameter is input, second is integer for onProgressUpdate, third is return for onPostExecute
+    private class FetchArtistsTask extends AsyncTask<String, Void, ArtistsPager> {
+
+        private final String LOG_TAG = FetchArtistsTask.class.getSimpleName();
+
+        /**
+         * The system calls doInBackground on a worker (background) thread
+         * @param params first and only element is artistName to make request to Spotify
+         * @return ArtistsPager SpotifyApi Java object.
+         */
+        @Override
+        protected ArtistsPager doInBackground(String... params) {
+
+            String artistName = params[0];
+
+            // don't use try/catch/finally here. Assume SpotifyAPI is handling it's errors.
+
+            // https://docs.google.com/presentation/d/1Q8LwzD5ODqirWG7K_e4sklE3fEFY_dr4kH4hfoRa0BQ/pub?start=false&loop=false&delayms=15000#slide=id.ga25585343_0_137
+            SpotifyApi spotifyApi = new SpotifyApi();
+            SpotifyService spotifyService = spotifyApi.getService();
+            ArtistsPager artistsPager = spotifyService.searchArtists(artistName);
+            return artistsPager;
+        }
+
+        /**
+         * Use to update UI. The system calls this on the UI thread.
+         * @param artistsPager is the result returned from doInBackground()
+         */
+        @Override
+        protected void onPostExecute(ArtistsPager artistsPager) {
+            super.onPostExecute(artistsPager);
+
+            // https://developer.spotify.com/web-api/object-model
+            List<Artist> artistList = artistsPager.artists.items;
+
+            ArrayList<String> artistsStrings = new ArrayList<String>();
+
+            for (Artist artist : artistList) {
+                // also can get artist.id
+                artistsStrings.add(artist.name);
+            }
+            updateAdapter(adapter, artistsStrings);
+        }
+
+        private void updateAdapter(ArrayAdapter anAdapter, ArrayList<String> artistsStrings) {
+            anAdapter.clear();
+            anAdapter.addAll(artistsStrings);
+        }
+
     }
 }
