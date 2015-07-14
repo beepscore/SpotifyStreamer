9f49ac2bf0921ef1827ca8ae867a6c5028dc73dc
beepscore <support@beepscore.com>
2015-07-13 17:49:40 -0700

    fix: Reviewer "Nitpick"- Consider raising minSDK to between 14-16.
    In fragment and layout xml, replace android.support.v7.widget.SearchView with import android.widget.SearchView
    SearchView was introduced in API 11, so app could have used standard non-support version for minSDK >= 11.

--- app/src/main/java/com/beepscore/android/spotifystreamer/ArtistsFragment.java
+++ app/src/main/java/com/beepscore/android/spotifystreamer/ArtistsFragment.java
@@ -4,12 +4,12 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
