7afb0e23f53876e9d3a2fbd766855277fc07f8d6
beepscore <support@beepscore.com>
2015-07-13 17:40:27 -0700

    fix: Reviewer "Nitpick"- Consider raising minSDK to between 14-16.
    Replace import android.support.v4.app.Fragment with android.app.Fragment

--- app/src/main/java/com/beepscore/android/spotifystreamer/TracksFragment.java
+++ app/src/main/java/com/beepscore/android/spotifystreamer/TracksFragment.java
@@ -1,8 +1,8 @@
 package com.beepscore.android.spotifystreamer;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
