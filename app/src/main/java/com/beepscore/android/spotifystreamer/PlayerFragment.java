1ecc06ecdc8f38b114b2276186094f9f0a53c73a
beepscore <support@beepscore.com>
2015-08-12 20:54:24 -0700

    feat: In PlayerFragment override onCreate and add configureTrackParcelable to get intent and trackParcelable.
    Also set action bar title to app name.

--- app/src/main/java/com/beepscore/android/spotifystreamer/PlayerFragment.java
+++ app/src/main/java/com/beepscore/android/spotifystreamer/PlayerFragment.java
@@ -1,22 +1,61 @@
 package com.beepscore.android.spotifystreamer;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * A placeholder fragment containing a simple view.
+ *
  */
 public class PlayerFragment extends Fragment {
+
+    private TrackParcelable trackParcelable;
 
     public PlayerFragment() {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // get the intent the activity was started with
+        // http://stackoverflow.com/questions/11387740/where-how-to-getintent-getextras-in-an-android-fragment
+        Intent intent = getActivity().getIntent();
+        configureTrackParcelable(intent);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_player, container, false);
+
+        View playerView = inflater.inflate(R.layout.fragment_player, container, false);
+
+        configureActionBarTitle(getString(R.string.app_name));
+
+        if (trackParcelable != null
+                && trackParcelable.albumName != null) {
+            // set name
+        }
+
+        return playerView;
+    }
+
+    private void configureTrackParcelable(Intent intent) {
+        String TRACK_KEY = getActivity().getString(R.string.TRACK_KEY);
+        if (intent != null && intent.hasExtra(TRACK_KEY)) {
+            trackParcelable = intent.getParcelableExtra(TRACK_KEY);
+        }
+    }
+
+    private void configureActionBarTitle(String title) {
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null
+                && activity.getSupportActionBar() != null) {
+            activity.getSupportActionBar().setTitle(title);
+        }
     }
 }
