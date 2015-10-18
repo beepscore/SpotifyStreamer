f539ded082f2efb309901afd224a48049309c454
beepscore <support@beepscore.com>
2015-10-17 23:12:39 -0700

    refactor: In ArtistsActivity rename DETAILFRAGMENT_TAG to ARTISTS_FRAGMENT_TAG and change its value.

--- app/src/main/java/com/beepscore/android/spotifystreamer/ArtistsActivity.java
+++ app/src/main/java/com/beepscore/android/spotifystreamer/ArtistsActivity.java
@@ -11,7 +11,7 @@ public class ArtistsActivity extends AppCompatActivity
         implements ArtistsFragment.Callback, TracksFragment.Callback, PlayerFragment.Callback {
 
     private final String LOG_TAG = ArtistsActivity.class.getSimpleName();
-    private static final String DETAILFRAGMENT_TAG = "DFTAG";
+    private static final String ARTISTS_FRAGMENT_TAG = "ARTISTS_FRAGMENT_TAG";
     private static final String PLAYERFRAGMENT_TAG = "PLAYERFRAGMENT_TAG";
     protected boolean mTwoPane;
 
@@ -26,7 +26,7 @@ public class ArtistsActivity extends AppCompatActivity
             // show the detail fragment in this activity
             // add or replace the detail fragment using a fragment transaction.
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.tracks_detail_container, new TracksFragment(), DETAILFRAGMENT_TAG)
+                    .replace(R.id.tracks_detail_container, new TracksFragment(), ARTISTS_FRAGMENT_TAG)
                     .commit();
         }
     }
@@ -73,7 +73,7 @@ public class ArtistsActivity extends AppCompatActivity
 
             // Use fragment transaction to replace the detail fragment
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.tracks_detail_container, fragment, DETAILFRAGMENT_TAG)
+                    .replace(R.id.tracks_detail_container, fragment, ARTISTS_FRAGMENT_TAG)
                     .commit();
 
         } else {
